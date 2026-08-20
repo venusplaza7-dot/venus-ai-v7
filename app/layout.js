767edbdb1ec2584f91e7ba9d1bd76527e778156b
Ron Kahn <venusplaza7@gmail.com>
2026-08-20 20:50:55 +0500

    FINAL FIX - no bom - no 404

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,1 @@
-﻿export default function Layout({children}){return <html><body style={{margin:0}}>{children}</body></html>}
+export default function Layout({children}){return <html><body style={{margin:0,fontFamily:'Arial'}}>{children}</body></html>}

c56eef00090946e9549db868aac5906b73ce821b
venusplaza7-dot <venusplaza7@gmail.com>
2026-08-27 22:13:37 +0500

    Update route.ts

--- app/api/real/route.ts
+++ app/api/real/route.ts
@@ -1,4 +1,4 @@
-mport { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { kv } from '@vercel/kv';
 
 export const dynamic = 'force-dynamic';
